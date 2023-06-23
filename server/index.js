84a00e2f1614aad4cfdb6f1ba1b9869b9d1bd279
rabirabii <wahyubudiman0624@gmail.com>
2023-06-23 18:25:39 +0700

    update app.js to index.js on server

--- server/index.js
+++ server/index.js
@@ -1,59 +1,60 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-// Import Routes
-const user = require("./controllers/user.js");
-const product = require("./controllers//product.js");
-const doctor = require("./controllers/doctor.js");
-const cart = require("./controllers/cart.js");
-const order = require("./controllers/order.js");
-const payment = require("./controllers/payment.js");
-// Data Imports
-
-/* Configuration  */
-dotenv.config();
+const ErrorHandler = require("./middleware/error");
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
 app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
   })
 );
-app.use("/", express.static(path.join(__dirname, "./uploads")));
-// Mongoose Setup
-const PORT = process.env.PORT || 9000;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () =>
-      console.log(
-        `Server Successfully Connected to : ${PORT}. Visit http://localhost:${PORT}/ In your browser`
-      )
-    );
 
-    app.use("/petshop/user", user); // Product.insertMany(dataProduct);
-    app.use("/petshop/product", product);
-    app.use("/petshop/doctor", doctor);
-    app.use("/petshop/cart", cart);
-    app.use("/petshop/order", order);
-    app.use("/petshop/payment", payment);
-    // ProductStat.insertMany(dataProductStat);
-    // User.insertMany(dataUser);
-    // Transaction.insertMany(dataTransaction);
-  })
-  .catch((error) => console.log(`${error} did not connect `));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/", express.static(path.join(__dirname, "./uploads")));
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import routes
+const user = require("./controller/user");
+const shop = require("./controller/shop");
+const doctor = require("./controller/doctor");
+const product = require("./controller/product");
+const event = require("./controller/event");
+const coupon = require("./controller/coupounCode");
+const payment = require("./controller/payment");
+const order = require("./controller/order");
+const conversation = require("./controller/conversation");
+const message = require("./controller/message");
+const withdraw = require("./controller/withdraw");
+const reservation = require("./controller/reservation");
+app.use("/api/petshop/user", user);
+app.use("/api/petshop/doctor", doctor);
+app.use("/api/petshop/conversation", conversation);
+app.use("/api/petshop/message", message);
+app.use("/api/petshop/order", order);
+app.use("/api/petshop/shop", shop);
+app.use("/api/petshop/product", product);
+app.use("/api/petshop/event", event);
+app.use("/api/petshop/coupon", coupon);
+app.use("/api/petshop/payment", payment);
+app.use("/api/petshop/withdraw", withdraw);
+// app.use("/api/petshop/reservation", reservation);
+// it's for ErrorHandling
+app.use(ErrorHandler);
+
+module.exports = app;
